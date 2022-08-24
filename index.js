3cc72802d829beb59df9af2a739d2ab1abe33ea2
MarcoValeri <marcovaleri@hotmail.it>
2022-08-24 16:57:02 +0100

    Connect the web app to the mysql db

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+// Import modules
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Connect to MySQL db
+const hostname = process.env.HOST;
+const connectDB = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: ''
+});
+
+connectDB.connect(err => {
+    if (err) throw err;
+    console.log(`Connected`);
+})
+
+// Set view
 app.set('view engine', 'ejs');
 
 // Routes
