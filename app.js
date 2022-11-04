896ef8850aa68057d286d8996f6cc69e9bb15f23
Marco Valeri <marcovaleri@hotmail.it>
2022-11-04 12:17:54 +0000

    Import new mvc project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Call the view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+// Controllers
+const errorsController = require('./controllers/error');
+
+// Parse npm install --save body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// 404 error page
+app.use(errorsController.get404);
+
+app.listen(3000);
