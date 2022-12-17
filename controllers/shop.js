169cfde4308eb24b837f132928fcc77b9e16e1d4
Marco Valeri <marcovaleri@hotmail.it>
2022-12-17 09:50:45 +0000

    Add single product

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 // Models
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     
@@ -45,6 +46,14 @@ exports.getCart = (req, res, next) => {
         path: '/cart',
         pageTitle: 'Your Cart'
     });
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    })
+    res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
