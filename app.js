be23d725757e543c7ee967bedb3d18a3ea165c18
Marco Valeri <marcovaleri@hotmail.it>
2023-01-14 18:26:34 +0000

    Add user mode

--- app.js
+++ app.js
@@ -16,10 +16,23 @@ const shopRoutes = require('./routes/shop');
 const errorsController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+// Modules
+const Product = require('./models/product');
+const User = require('./models/user');
+
 // Parse npm install --save body-parser
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -27,10 +40,23 @@ app.use(shopRoutes);
 // 404 error page
 app.use(errorsController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 // Sequelize
 sequelize
+    // .sync({ force: true })
     .sync()
     .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({name: 'Max', email: 'test@test.com' });
+        }
+        return user;
+    })
+    .then(user => {
         app.listen(3000);
     })
     .catch(err => {
